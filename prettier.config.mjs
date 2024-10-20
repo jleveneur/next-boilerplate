f6ecfe4cf3c4099f610fcd04a82b5fe08a538566
Julien Leveneur <jleveneur.pro@gmail.com>
2024-10-20 21:21:15 +0000

    Refactor eslint configuration to include boundaries plugin

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -11,6 +11,7 @@ const config = {
   importOrder: [
     '^(react|next?/?([a-zA-Z/]*))$',
     '<THIRD_PARTY_MODULES>',
+    '^@/features(.*)$',
     '^@/components(.*)$',
     '^@/lib(.*)$',
     '^@/styles(.*)$',
